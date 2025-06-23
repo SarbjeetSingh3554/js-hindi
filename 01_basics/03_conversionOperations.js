e79299b7520a29d022111f5c86ca5828ff4ba8ee
SarbjeetSingh3554 <sarbjeetsingh9416@gmail.com>
2025-06-23 10:31:49 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+
+//"33" => 33
+//"33abc" => NaN NOT A NUMBER WITH TYPE NUMBER
+//true => 1
+// false => 0

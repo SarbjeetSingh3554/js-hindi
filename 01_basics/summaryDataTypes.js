12004faf97bb4a546d4b27bf583156e87b9273d6
SarbjeetSingh3554 <sarbjeetsingh9416@gmail.com>
2025-06-26 10:17:38 +0000

    dataType summary

---
+++ 01_basics/summaryDataTypes.js
@@ -0,0 +1,34 @@
+// on the basics of how data is stored in memoryans accessed can be devided as follows:-
+
+// Primitive
+
+// 7 type : string,Number ,Bool ,Null ,Undefined ,Symbol ,BigInt
+
+
+const score = 100
+const scoreval = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber = 2342423423431n // Bigint by typing n at last
+
+// referance typ/non=primitive
+
+// Array ,Object ,Functions
+
+const heros=["shaktiman","superman"]
+let myObj = {
+    name: "sarb",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("whsts up")
+}
+

6562295e4bc4586e7ed9d5cb2947acc616be7f8e
SarbjeetSingh3554 <sarbjeetsingh9416@gmail.com>
2025-06-26 09:57:05 +0000

    operators

--- 01_basics/02_variables.js
+++ 01_basics/02_variables.js
@@ -3,7 +3,7 @@ let accountEmail="123@gmail.com"
 var accountPassword="12345"
 accountCity = "Jaipur"
 
-//accountId = 2. is not allowed
+//accountId = 2 // is not allowed
 
 accountEmail="abs@gmail.com"
 accountPassword="123"
--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,13 +1,51 @@
-let score = null
+// let score = null
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let value = Number(score)
-console.log(typeof value)
-console.log(value)
+// let value = Number(score)
+// console.log(typeof value)
+// console.log(value)
 
 //"33" => 33
 //"33abc" => NaN NOT A NUMBER WITH TYPE NUMBER
 //true => 1
 // false => 0
+
+
+//*******************************Operations**************************//
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// let str1="hello"
+// let str2="sarb"
+// let str3=str1+" "+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+// console.log(true+)
+
+// let num1,num2,num3
+
+// num1=num2=num3=2*2
+
+// let gamecounter = 100
+// gamecounter++
+// console.log(gamecounter)
+
